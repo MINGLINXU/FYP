5216f276879e085fdbbddef55794740e9d24adce
18037689 <Zh3nn4n>
2020-06-06 17:47:32 +0800

    Additional activities

---
+++ lite/examples/image_classification/android/app/src/main/java/org/tensorflow/lite/examples/classification/IdentificationDetailsActivity.java
@@ -0,0 +1,14 @@
+package org.tensorflow.lite.examples.classification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IdentificationDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_identification_details);
+    }
+}
---
+++ lite/examples/image_classification/android/app/src/main/java/org/tensorflow/lite/examples/classification/LoginActivity.java
@@ -0,0 +1,28 @@
+package org.tensorflow.lite.examples.classification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, ClassifierActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
